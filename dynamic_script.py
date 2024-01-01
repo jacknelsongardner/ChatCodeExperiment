998eccdcb8c1ec47b6cb732c5743f65b71b469af
Jack Gardner <shortfilmsbyjack@aol.com>
2024-01-01 14:43:51 -0800

    Implemented function for using GPT API
    Implemented a method that gets responses from openai api with davinci-3. (I didn't include the ENV file with the api code, for obvious privacy concerns)

--- dynamic_script.py
+++ dynamic_script.py
@@ -1,4 +1,26 @@
+from dotenv import load_dotenv
 import os
+import openai
+
+api_key = "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"
+openai.api_key = api_key
+
+prompt = """
+I have a system in place to write and delete code files on a hard drive from you. 
+The program is running on macOS. 
+I need you to not give any comments or anything other than the commands necessary to do whatever you want. 
+Your available commands are: NEWFILE : (path of new file) : (text for new file), 
+TERMINAL COMMAND : (command you want to enter in terminal)
+"""
+
+response = openai.Completion.create(
+    engine="text-davinci-003",
+    prompt=prompt,
+    max_tokens=100
+)
+
+generated_code = response['choices'][0]['text']
+print(generated_code)
 
 working_path: str = os.getcwd()
 
@@ -38,6 +60,8 @@ def parse_commands(commands):
             parsed_commands.append((command,contents))
 
     return parsed_commands
+
+
 
 def test():
     # Define the script content (you can modify this as needed)
